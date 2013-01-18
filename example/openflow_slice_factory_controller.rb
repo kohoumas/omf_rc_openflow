08c501caef17417a657e675eeab14e1c8047a206
kohoumas <kohoumas@gmail.com>
2013-01-18 14:19:51 +0200

    change version to .pre

--- example/openflow_slice_factory_controller.rb
+++ example/openflow_slice_factory_controller.rb
@@ -2,29 +2,27 @@
 
 require 'omf_rc'
 require 'omf_rc/resource_factory'
+require 'omf_rc_openflow'
 $stdout.sync = true
 
 Blather.logger = logger
 
 opts = {
   # XMPP server domain
-  server: 'srv.mytestbed.net', # 'localhost',
+  server: 'srv.mytestbed.net',
+  user: 'flowvisor',
+  password: 'pw',
+  uid: 'flowvisor',
   # Debug mode of not
   debug: false
 }
 
 Logging.logger.root.level = :debug if opts[:debug]
 
-OmfRc::ResourceFactory.load_addtional_resource_proxies("../lib/omf_rc/resource_proxy")
-
 EM.run do
   # Use resource factory method to initialise a new instance of garage
-  g = "flowvisor"
-  info "Starting #{g}"
-  flowvisor = OmfRc::ResourceFactory.new(
-    :openflow_slice_factory,
-    opts.merge(user: g, password: 'pw', uid: g)
-  )
+  info "Starting #{opts[:uid]}"
+  flowvisor = OmfRc::ResourceFactory.new(:openflow_slice_factory, opts)
   flowvisor.connect
 
   # Disconnect garage from XMPP server, when these two signals received
--- lib/omf_rc_openflow/version.rb
+++ lib/omf_rc_openflow/version.rb
@@ -1,3 +1,3 @@
 module OmfRcOpenflow
-  VERSION = "6.0.0"
+  VERSION = "6.0.0.pre"
 end
