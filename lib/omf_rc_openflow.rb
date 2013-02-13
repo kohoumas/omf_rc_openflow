f950034ccbedbe48f6357ac164b09aeccee2a4d1
kohoumas <kohoumas@gmail.com>
2013-02-13 15:54:21 +0200

    add Virtual Openflow Switch Factory

--- lib/omf_rc_openflow.rb
+++ lib/omf_rc_openflow.rb
@@ -2,4 +2,7 @@ require "omf_rc_openflow/version"
 
 require 'omf_rc/resource_proxy/openflow_slice_factory.rb'
 require 'omf_rc/resource_proxy/openflow_slice.rb'
-require 'omf_rc/util/openflow_tools.rb'
+require 'omf_rc/util/openflow_slice_tools.rb'
+require 'omf_rc/resource_proxy/virtual_openflow_switch_factory.rb'
+require 'omf_rc/resource_proxy/virtual_openflow_switch.rb'
+require 'omf_rc/util/virtual_openflow_switch_tools.rb'
